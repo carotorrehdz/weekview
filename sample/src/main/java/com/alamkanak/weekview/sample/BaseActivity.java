cc0ea8816be0a07441085335facf3184482b207c
carotorrehdz <cdelatorre@Carolinas-MacBook-Pro.local>
2017-10-02 16:05:34 -0500

    Sample

--- sample/src/main/java/com/alamkanak/weekview/sample/BaseActivity.java
+++ sample/src/main/java/com/alamkanak/weekview/sample/BaseActivity.java
@@ -24,132 +24,88 @@ import java.util.Locale;
  * Created by Raquib-ul-Alam Kanak on 1/3/2014.
  * Website: http://alamkanak.github.io
  */
-public abstract class BaseActivity extends AppCompatActivity implements WeekView.EventClickListener, MonthLoader.MonthChangeListener, WeekView.EventLongPressListener, WeekView.EmptyViewLongPressListener {
-    private static final int TYPE_DAY_VIEW = 1;
-    private static final int TYPE_THREE_DAY_VIEW = 2;
-    private static final int TYPE_WEEK_VIEW = 3;
-    private int mWeekViewType = TYPE_WEEK_VIEW;
-    private WeekView mWeekView;
+public abstract class BaseActivity extends AppCompatActivity implements WeekView.EventClickListener, MonthLoader.MonthChangeListener, WeekView.EventLongPressListener, WeekView.EmptyViewLongPressListener, WeekView.EmptyViewClickListener, WeekView.ScrollListener, DateTimeInterpreter {
 
+    private WeekView mWeekView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
-        // Get a reference for the week view in the layout.
         mWeekView = (WeekView) findViewById(R.id.weekView);
 
-        // Show a toast message about the touched event.
         mWeekView.setOnEventClickListener(this);
 
-        // The week view has infinite scrolling horizontally. We have to provide the events of a
-        // month every time the month changes on the week view.
         mWeekView.setMonthChangeListener(this);
 
-        // Set long press listener for events.
         mWeekView.setEventLongPressListener(this);
 
-        // Set long press listener for empty view
         mWeekView.setEmptyViewLongPressListener(this);
 
-        // Set up a date time interpreter to interpret how the date and time will be formatted in
-        // the week view. This is optional.
-        setupDateTimeInterpreter();
-    }
+        mWeekView.setEmptyViewClickListener(this);
 
+        mWeekView.setScrollListener(this);
+
+        mWeekView.setDateTimeInterpreter(this);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(1506747600000L);
+        mWeekView.goToDate(calendar);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        setupDateTimeInterpreter();
+        switch (item.getItemId()) {
+            case R.id.action_today:
+                mWeekView.goToToday();
 
-        switch (id) {
-//            case R.id.action_today:
-//                mWeekView.goToToday();
-//                return true;
-//            case R.id.action_day_view:
-//                if (mWeekViewType != TYPE_DAY_VIEW) {
-//                    item.setChecked(!item.isChecked());
-//                    mWeekViewType = TYPE_DAY_VIEW;
-//                    mWeekView.setNumberOfVisibleDays(1);
-//
-//                    // Lets change some dimensions to best fit the view.
-//                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
-//                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
-//                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
-//                }
-//                return true;
-//            case R.id.action_three_day_view:
-//                if (mWeekViewType != TYPE_THREE_DAY_VIEW) {
-//                    item.setChecked(!item.isChecked());
-//                    mWeekViewType = TYPE_THREE_DAY_VIEW;
-//                    mWeekView.setNumberOfVisibleDays(3);
-//
-//                    // Lets change some dimensions to best fit the view.
-//                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
-//                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
-//                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
-//                }
-//                return true;
-            case R.id.action_week_view:
-//                if (mWeekViewType != TYPE_WEEK_VIEW) {
-//                    item.setChecked(!item.isChecked());
-//                    mWeekViewType = TYPE_WEEK_VIEW;
-
-                // Lets change some dimensions to best fit the view.
-//                mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
-//                mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
-//                }
                 return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void setupDateTimeInterpreter() {
-        mWeekView.setDateTimeInterpreter(new DateTimeInterpreter() {
-            @Override
-            public String interpretWeekday(Calendar date) {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("EEE", Locale.getDefault());
-                String weekday = dateFormat.format(date.getTime());
+    @Override
+    public String interpretWeekday(Calendar date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE", Locale.getDefault());
+        String weekday = dateFormat.format(date.getTime());
 
-                return WordUtils.capitalize(weekday).replace(".", "");
-            }
+        return WordUtils.capitalize(weekday).replace(".", "");
+    }
 
-            @Override
-            public String interpretDay(Calendar date) {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("d", Locale.getDefault());
+    @Override
+    public String interpretDay(Calendar date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d", Locale.getDefault());
 
-                return dateFormat.format(date.getTime());
-            }
+        return dateFormat.format(date.getTime());
+    }
 
-            @Override
-            public String interpretTime(int hour) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(Calendar.HOUR_OF_DAY, hour);
-                calendar.clear(Calendar.MINUTE);
-                SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm", Locale.ENGLISH);
+    @Override
+    public String interpretTime(int hour) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.clear(Calendar.MINUTE);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm", Locale.ENGLISH);
 
-                return dateFormat.format(calendar.getTime()).toLowerCase();
-            }
+        return dateFormat.format(calendar.getTime()).toLowerCase();
+    }
 
-            @Override
-            public String interpretPeriod(int hour) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(Calendar.HOUR_OF_DAY, hour);
-                calendar.clear(Calendar.MINUTE);
-                SimpleDateFormat dateFormat = new SimpleDateFormat("a", Locale.ENGLISH);
+    @Override
+    public String interpretPeriod(int hour) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.clear(Calendar.MINUTE);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("a", Locale.ENGLISH);
 
-                return dateFormat.format(calendar.getTime()).toLowerCase();
-            }
-        });
+        return dateFormat.format(calendar.getTime()).toLowerCase();
     }
 
     protected String getEventTitle(Calendar time) {
@@ -167,8 +123,30 @@ public abstract class BaseActivity extends AppCompatActivity implements WeekView
     }
 
     @Override
+    public void onEmptyViewClicked(Calendar time) {
+        Toast.makeText(this, "Empty view: " + getEventTitle(time), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
     public void onEmptyViewLongPress(Calendar time) {
         Toast.makeText(this, "Empty view long pressed: " + getEventTitle(time), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onFirstVisibleDayChanged(Calendar newFirstVisibleDay, Calendar oldFirstVisibleDay) {
+        if (newFirstVisibleDay == null || oldFirstVisibleDay == null) {
+            return;
+        }
+
+        int newMonth = newFirstVisibleDay.get(Calendar.MONTH);
+        int oldMonth = oldFirstVisibleDay.get(Calendar.MONTH);
+
+        if (newMonth == oldMonth) {
+            return;
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM", Locale.getDefault());
+        setTitle(dateFormat.format(newFirstVisibleDay.getTime()));
     }
 
     public WeekView getWeekView() {
